e319cc6f9fa01b76c9fee8a082b4a8c74ddbd5a8
Jerquanus Swann <jerquanus@Jerquanuss-Air.lan>
2022-06-28 18:34:42 -0400

    Null is showing up in console.log now

--- src/components/MoonPhaseDisplay.js
+++ src/components/MoonPhaseDisplay.js
@@ -1,96 +1,96 @@
-// import "./style.css"
+// // import "./style.css"
 
-export default function MoonPhaseDisplay(props) {
-    console.log(props.moon.moonphase, "THE GOODS--")
-    const lunar = props.moon.moonphase;
-    // ----- condtional rendering will be used -----
-    // added optional chaining to theMessages
-    const theMessages = lunar && lunar?.messages;
+// export default function MoonPhaseDisplay(props) {
+//     console.log(props.moon.moonphase, "THE GOODS--")
+//     const lunar = props.moon.moonphase;
+//     // ----- condtional rendering will be used -----
+//     // added optional chaining to theMessages
+//     const theMessages = lunar && lunar?.messages;
 
-    // Displaying the Date
-    // const date = () => {
-    //     // would convert date using moment--
-    //     const theDate = lunar.conversationDate;
-    //     const newDate= moment(theDate).format("dddd, MMMM Do YYYY");
-    //     console.log(theDate, "what time");
+//     // Displaying the Date
+//     // const date = () => {
+//     //     // would convert date using moment--
+//     //     const theDate = lunar.conversationDate;
+//     //     const newDate= moment(theDate).format("dddd, MMMM Do YYYY");
+//     //     console.log(theDate, "what time");
 
-    //     return (
-    //         <div className="wrapper">
-    //             <div className="Lineup">
-    //                 {/* flexbox or grid to seperate the date and header to display on one line */}
-    //                 <div className="Headline" style={{color:"#ee8065d2"}}><h1>CHAT lunar</h1></div>
-    //                 <div className="Date" style={{color:"#9bd0e5"}}><h1> {newDate}</h1></div>
-    //             </div>            
-    //         </div>
-    //     );
-
-
-    // };
-
-    // Displaying the chat
-    const chatMessages = () => {
+//     //     return (
+//     //         <div className="wrapper">
+//     //             <div className="Lineup">
+//     //                 {/* flexbox or grid to seperate the date and header to display on one line */}
+//     //                 <div className="Headline" style={{color:"#ee8065d2"}}><h1>CHAT lunar</h1></div>
+//     //                 <div className="Date" style={{color:"#9bd0e5"}}><h1> {newDate}</h1></div>
+//     //             </div>            
+//     //         </div>
+//     //     );
 
 
-        return (
-                <div className="Container">
+//     // };
 
-                        <div className="MygelAvatar1">
-                            <img src= {theMessages[0]?.image} style={{float: 'left', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
-                            className="HaloM"/>
-                        </div>
-
-                        <div className="MygelChat1">
-                            <p>{theMessages[0]?.message}</p>
-                            <h3 style={{color:"#c7754f"}}>{theMessages[0]?.username}</h3>
-                            {/* --- timestamp conversion needed using moment but since its not mapped over difficulties converting ---*/}
-                            <p>{theMessages[0]?.timestamp}</p>
-                        </div>
-
-                        <div className="CharlieAvatar1">
-                            <img src= {theMessages[1]?.image} style={{float: 'right', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
-                            className="HaloC"/>
-                        </div>
-
-                        <div className="CharlieChat1">
-                            <p>{theMessages[1]?.message}</p>
-                            {/* --- timestamp conversion needed using moment (npm install)  ---*/}
-                            <h3 style={{color:"#c7754f"}}>{theMessages[1]?.username}</h3>
-                            <p>{theMessages[1]?.timestamp}</p>
-                        </div>
-
-                        <div  className="MygelAvatar2">
-                            <img src= {theMessages[2]?.image} style={{float: 'left', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
-                            className="HaloM"/>
-                        </div>
-
-                        <div className="MygelChat2">
-                             <p>{theMessages[2]?.message}</p>
-                                {/* --- timestamp conversion needed using moment(npm install) ---*/}
-                            <h3 style={{color:"#c7754f"}}>{theMessages[2]?.username}</h3>
-                            <p>{theMessages[2]?.timestamp}</p>
-                        </div>
-
-                        <div className="CharlieAvatar2">
-                                <img src= {theMessages[3]?.image} style={{float: 'right', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
-                                className="HaloC"/>
-                        </div>
-
-                        <div className="CharlieChat2">    
-                             <p>{theMessages[3]?.message}</p>
-                                {/* --- timestamp conversion needed using moment (npm install)  ---*/}
-                            <h3 style={{color:"#c7754f"}}>{theMessages[3]?.username}</h3>
-                            <p>{theMessages[3]?.timestamp}</p>
-                        </div>
-                </div>  
-        );
-    };
+//     // Displaying the chat
+//     const chatMessages = () => {
 
 
+//         return (
+//                 <div className="Container">
 
-    return (
-        <>
-        {/* {date(props)} */}
-        {chatMessages(props)}
-        </>
-    )
-};
+//                         <div className="MygelAvatar1">
+//                             <img src= {theMessages[0]?.image} style={{float: 'left', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
+//                             className="HaloM"/>
+//                         </div>
+
+//                         <div className="MygelChat1">
+//                             <p>{theMessages[0]?.message}</p>
+//                             <h3 style={{color:"#c7754f"}}>{theMessages[0]?.username}</h3>
+//                             {/* --- timestamp conversion needed using moment but since its not mapped over difficulties converting ---*/}
+//                             <p>{theMessages[0]?.timestamp}</p>
+//                         </div>
+
+//                         <div className="CharlieAvatar1">
+//                             <img src= {theMessages[1]?.image} style={{float: 'right', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
+//                             className="HaloC"/>
+//                         </div>
+
+//                         <div className="CharlieChat1">
+//                             <p>{theMessages[1]?.message}</p>
+//                             {/* --- timestamp conversion needed using moment (npm install)  ---*/}
+//                             <h3 style={{color:"#c7754f"}}>{theMessages[1]?.username}</h3>
+//                             <p>{theMessages[1]?.timestamp}</p>
+//                         </div>
+
+//                         <div  className="MygelAvatar2">
+//                             <img src= {theMessages[2]?.image} style={{float: 'left', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
+//                             className="HaloM"/>
+//                         </div>
+
+//                         <div className="MygelChat2">
+//                              <p>{theMessages[2]?.message}</p>
+//                                 {/* --- timestamp conversion needed using moment(npm install) ---*/}
+//                             <h3 style={{color:"#c7754f"}}>{theMessages[2]?.username}</h3>
+//                             <p>{theMessages[2]?.timestamp}</p>
+//                         </div>
+
+//                         <div className="CharlieAvatar2">
+//                                 <img src= {theMessages[3]?.image} style={{float: 'right', borderRadius :'180px', padding:'10px'}} alt="User Avatar"
+//                                 className="HaloC"/>
+//                         </div>
+
+//                         <div className="CharlieChat2">    
+//                              <p>{theMessages[3]?.message}</p>
+//                                 {/* --- timestamp conversion needed using moment (npm install)  ---*/}
+//                             <h3 style={{color:"#c7754f"}}>{theMessages[3]?.username}</h3>
+//                             <p>{theMessages[3]?.timestamp}</p>
+//                         </div>
+//                 </div>  
+//         );
+//     };
+
+
+
+//     return (
+//         <>
+//         {/* {date(props)} */}
+//         {chatMessages(props)}
+//         </>
+//     )
+// };
--- src/components/Moonimport.js
+++ src/components/Moonimport.js
@@ -10,35 +10,80 @@ function Moonphases () {
     
     const api = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/washington%20DC?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cmoonphase&include=days&key=PZ29AWQYWB27ZVT2XE8Z689AY&contentType=json"
     
-    
-    // let sharePhases = null
     useEffect(() => {
-        axios.get(api)
-        .then ((response) => {
-            console.log(response.data.days)
-            setPhases(response.data)
-        });
-        
+
+        async function getMoons() {
+            axios.get(api)
+            .then ((response) => {
+                console.log(response.data.days)
+                setPhases(response.data)
+            });
+        }  
+        getMoons();
     }, [api]);
+
+    console.log(phases,"The goods")
 
    
     // phases.map(item=>console.log(item.))
-
-
-    return(  
-        <div>
-            <h1>Pull MOON PHASES</h1> {
-             phases.map((moon) => (
-                <div key = {moon.days}>
-                    <p>{moon.days.moonphase}</p>
-                </div>
-                ))
-            }
-                      
-
-        </div>
-    )
+    if (phases){
+        return(  
+    
+            <div>
+                <h1>Pull MOON PHASES</h1> {
+                 phases.map((moon) => (
+                    <div key = {moon.data.days}>
+                        <p>{moon.days.moonphase}</p>
+                    </div>
+                    ))
+                }
+            </div>
+        )
+    }
 };
+
+
+// import {useState, useEffect} from 'react';
+
+// function App() {
+//   // ✅ State is initialized to `[]`
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     async function getUsers() {
+//       const response = await fetch('https://randomuser.me/api/', {
+//         method: 'GET',
+//         headers: {
+//           accept: 'application/json',
+//         },
+//       });
+
+//       const data = await response.json();
+
+//       setUsers(data.results);
+//     }
+
+//     getUsers();
+//   }, []);
+
+//   console.log(users);
+
+//   return (
+//     <div>
+//       {/* ✅ users is `[]`, until API responds */}
+//       {users.map(user => (
+//         <div key={user.id.value}>
+//           <h2>
+//             Name: {user.name.first} {user.name.last}
+//           </h2>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 
 // return (
 //     <div className = "App">
